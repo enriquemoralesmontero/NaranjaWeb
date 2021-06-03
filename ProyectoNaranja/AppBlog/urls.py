e740e6879d724d8ac1faee737d06e3aa6912a3d7
enriquemoralesmontero <48988427+enriquemoralesmontero@users.noreply.github.com>
2021-06-03 13:36:51 +0200

    Vídeo 41-42 --> Blog 100% (Categorías con links)

--- ProyectoNaranja/AppBlog/urls.py
+++ ProyectoNaranja/AppBlog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name="Blog"),
+    path('categoria/<categoria_id>', views.categoria, name='categoria'),  # Lo que va entre <> está pasado por parámetro.
 ]
--- ProyectoNaranja/AppBlog/views.py
+++ ProyectoNaranja/AppBlog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from AppBlog.models import Post
+from AppBlog.models import Post, Categoria
 
 # Create your views here.
 
@@ -7,3 +7,8 @@ def blog(request):
     """ Esta vista busca su plantilla HTML en 'ProyectoNaranja/AppBlog/templates/AppBlog/blog.html' """
     posts = Post.objects.all()  # Importa todos los posts existentes (todos los objetos de la clase Post).
     return render(request, "AppBlog/blog.html", {"posts": posts})
+
+def categoria(request, categoria_id):  # Le pasamos el id de categoria por parámetro para filtrar. Ver urls.py
+    categoria = Categoria.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias = categoria)
+    return render(request, "AppBlog/categoria.html", {"categoria": categoria, "posts": posts})
