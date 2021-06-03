1989757fd7aac50b5a9485b9a47297f423a4132e
enriquemoralesmontero <48988427+enriquemoralesmontero@users.noreply.github.com>
2021-06-03 23:40:31 +0200

    Vídeo 45 --> Contacto 50% (Guardar datos del formulario en variables)

--- ProyectoNaranja/AppContacto/urls.py
+++ ProyectoNaranja/AppContacto/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views  # = from AppContacto import views
 
 urlpatterns = [
-    path('contacto', views.contacto, name="Contacto"),
+    path('', views.contacto, name="Contacto"),
 ]
--- ProyectoNaranja/AppContacto/views.py
+++ ProyectoNaranja/AppContacto/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormularioContacto
 
 # Create your views here.
 
 def contacto(request):
     """ Esta vista busca su plantilla HTML en 'ProyectoNaranja/AppContacto/templates/AppContacto/contacto.html' """
-    formulario_contacto = FormularioContacto()
+    formulario_contacto = FormularioContacto()  # Si ha entrado en la web sin darle al submit.
+    if request.method == "POST":                # Si le ha dado al botón submit.
+        formulario_contacto = FormularioContacto(data = request.POST)
+        if formulario_contacto.is_valid():
+            nombre    = request.POST.get("nombre")  # ¡¡Datos del formulario pasados a una variable!!
+            email     = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+            return redirect("/contacto/?ok")
     return render(request, "AppContacto/contacto.html", {'miformulario':formulario_contacto})
