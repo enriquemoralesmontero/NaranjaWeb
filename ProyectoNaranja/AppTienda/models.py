c477ee229e31c130320349cef96435db938808f8
enriquemoralesmontero <48988427+enriquemoralesmontero@users.noreply.github.com>
2021-06-05 23:43:39 +0200

    Vídeo 48 --> Tienda 10% (Modelos creados y migrados a PostgreDB)

--- ProyectoNaranja/AppTienda/models.py
+++ ProyectoNaranja/AppTienda/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre  = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)  # Se inserta de manera automática
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name        = "categoria"
+        verbose_name_plural = "categorias"
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre         = models.CharField(max_length=50)
+    categorias     = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    imagen         = models.ImageField(upload_to="tienda", null=True, blank=True)
+    precio         = models.FloatField()
+    disponibilidad = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name        = "producto"
+        verbose_name_plural = "productos"
+
+    def __str__(self):
+        return self.nombre
