be280331177fdeee76eb839b460d7d9929b6b855
enriquemoralesmontero <48988427+enriquemoralesmontero@users.noreply.github.com>
2021-06-10 23:15:06 +0200

    Vídeo 54 --> Tienda 68% (Botones añadidos pero no funcionales)

--- ProyectoNaranja/AppCarro/views.py
+++ ProyectoNaranja/AppCarro/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from AppTienda.models import Producto
+from .carro import Carro
 
 # Create your views here.
+
+def agregar_producto(request, producto_id):
+
+    carro = Carro(request)
+    producto = Producto.objects.get(id = producto_id)
+    carro.agregar(producto = producto)
+    return redirect("tienda")
+
+
+def eliminar_producto(request, producto_id):
+
+    carro = Carro(request)
+    producto = Producto.objects.get(id=producto_id)
+    carro.eliminar(producto=producto)
+    return redirect("tienda")
+
+
+def restar_producto(request, producto_id):
+
+    carro = Carro(request)
+    producto = Producto.objects.get(id=producto_id)
+    carro.restar_productos(producto=producto)
+    return redirect("tienda")
+
+
+def limpiar_carro(request, producto_id):
+
+    carro = Carro(request)
+    carro.limpiar_carro()
+    return redirect("tienda")
